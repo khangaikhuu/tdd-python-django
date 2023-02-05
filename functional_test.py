5349dd8b8343a0ffa70259b8f0f9dac2c0a95059
khangaikhuu uvgunkhuu <khangaikhuu@gmail.com>
2023-02-05 20:49:13 +0800

    update with functional test

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,4 @@
 from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+assert 'The install worked successfully! Congratulations!' in browser.title
